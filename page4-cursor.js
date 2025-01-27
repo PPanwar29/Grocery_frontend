e7f5f2d70843a1985d39318ddeeb4f181179ee4c
PPanwar29 <prashantpanwar2972@gmail.com>
2025-01-27 17:42:30 +0530

    page4 added

---
+++ page4-cursor.js
@@ -0,0 +1,26 @@
+export function cursoranimation (){
+    var videocon = document.querySelector("#page4-inner")
+    var playbtn = document.querySelector("#cursor")
+    videocon.addEventListener("mouseenter", function(){
+        gsap.to(playbtn,{
+            transform: 'translate(-50%,-50%) scale(1)'
+        })
+    })
+
+
+    videocon.addEventListener("mouseleave", function(){
+        gsap.to(playbtn,{
+            scale: 0,
+            opacity: 0
+        })
+    })
+
+
+    videocon.addEventListener("mousemove", function(dets){
+        gsap.to(playbtn,{
+            left: dets.x+50,
+            top: dets.y+50,
+        })
+    })
+}
+cursoranimation()
